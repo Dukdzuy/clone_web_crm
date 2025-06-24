2696216bd88dc5a70d8865a12a32319078c83952
Dukdzuy <133631863+Dukdzuy@users.noreply.github.com>
2025-06-24 17:20:01 +0700

    Add files via upload

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
-import LeadTable from './lead';
-import { DASH_BOARD, LeadList } from './configs/path';
+import LeadTable from './pages/lead/lead';
+import { DASH_BOARD, LEAD_COMMON, LEAD_LIST, TO_DELIVER_LIST } from './configs/path';
 import PublicLayout from './layout/PublicLayout';
 import PrivateLayout from './layout/PrivateLayout';
 import AuthPage from './pages/auth';
+import LeadList from './pages/lead/leadlist';
+import ToDeliverList from './pages/lead/todeliver';
 
 const AppRouter: React.FC = () => {
   return (
@@ -18,10 +20,26 @@ const AppRouter: React.FC = () => {
         }
       />
       <Route
-        path={LeadList}
+        path={LEAD_COMMON}
         element={
           <PrivateLayout>
             <LeadTable />
+          </PrivateLayout>
+        }
+      />
+      <Route
+        path={LEAD_LIST}
+        element={
+          <PrivateLayout>
+            <LeadList />
+          </PrivateLayout>
+        }
+      />
+      <Route
+        path={TO_DELIVER_LIST}
+        element={
+          <PrivateLayout>
+            <ToDeliverList />
           </PrivateLayout>
         }
       />
